c6e5faf97a33bdc2d48ab7a548b5bed553eaa9b7
MourishAntony-2024-aib <mourishantonyc@gmail.com>
2024-11-16 20:15:07 +0530

    Delete demo.py

--- demo.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-selected = st.selectbox('Select one option:', ['', 'First one', 'Second one'], format_func=lambda x: 'Select an option' if x == '' else x)
-
-if selected:
-    st.success('Yay! 🎉')
-else:
-    st.warning('No option is selected')
